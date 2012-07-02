5de2e87f34dace49994b599be52955605cfacfac
Yanick Champoux <yanick@babyl.dyndns.org>
2012-07-02 12:31:59 -0400

    documentation

--- lib/WWW/Ohloh/API/Object/Account.pm
+++ lib/WWW/Ohloh/API/Object/Account.pm
@@ -1,4 +1,17 @@
 package WWW::Ohloh::API::Object::Account;
+#ABSTRACT: an Ohloh account
+
+=head1 SYNOPSIS
+
+    use WWW::Ohloh::API;
+
+    my $ohloh = WWW::Ohloh::API->new( api_key => $my_api_key );
+
+    my $account = $ohloh->fetch( 'Account', id => 12933 );
+
+    print $account->name;
+
+=cut
 
 use Moose;
 
@@ -12,13 +25,9 @@ with qw/
 use WWW::Ohloh::API::Types qw/ OhlohId OhlohDate OhlohURI /;
 
 use Carp;
-use XML::LibXML;
-use Time::Piece;
 use Date::Parse;
 
 use Digest::MD5 qw/ md5_hex /;
-
-our $VERSION = '1.0_1';
 
 use overload '""' => sub { $_[0]->name  };
 
@@ -149,6 +158,13 @@ has 'kudo_score' => (
     },
 );
 
+=method stack
+
+Returns the L<WWW::Ohloh::API::Object::Stack> object associated with the
+account.
+
+=cut
+
 has stack => (
     is => 'rw',
     isa => 'WWW::Ohloh::API::Object::Stack',
@@ -163,7 +179,6 @@ has stack => (
         );
     },
 );
-
 
 around _build_request_url => sub {
     my( $inner, $self ) = @_;
@@ -210,146 +225,12 @@ C<id>, C<email> or C<email_md5>.
 1;
 
 __END__
-#<<<
-my @kudo_of             : Field 
-                        : Set(_set_kudo) 
-                        : Get(kudo_score)
-                        ;
-#>>>
-my @kudos_of : Field : Arg(kudos);
-
-my @stack : Field;
-
-
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-sub load_xml {
-    my ( $self, $dom ) = @_;
-
-    $self->_set_id( $dom->findvalue('id/text()') );
-    $self->_set_name( $dom->findvalue('name/text()') );
-    $self->_set_created_at(
-        scalar Time::Piece::gmtime(
-            str2time( $dom->findvalue('created_at/text()') ) ) );
-    $self->_set_updated_at(
-        scalar Time::Piece::gmtime(
-            str2time( $dom->findvalue('updated_at/text()') ) ) );
-    $self->_set_homepage_url( $dom->findvalue('homepage_url/text()') );
-    $self->_set_avatar_url( $dom->findvalue('avatar_url/text()') );
-    $self->_set_posts_count( $dom->findvalue('posts_count/text()') );
-    $self->_set_location( $dom->findvalue('location/text()') );
-    $self->_set_country_code( $dom->findvalue('country_code/text()') );
-    $self->_set_latitude( $dom->findvalue('latitude/text()') );
-    $self->_set_longitude( $dom->findvalue('longitude/text()') );
-
-    if ( my ($node) = $dom->findnodes('kudo_score[1]') ) {
-        $kudo_of[$$self] = WWW::Ohloh::API::KudoScore->new( xml => $node );
-    }
-}
-
-sub kudoScore {
-    my $self = shift;
-    return $kudo_of[$$self];
-}
-
-# aliases
-*kudo = *kudoScore;
-
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-sub stack {
-    my $self = shift;
-
-    my $retrieve = shift;
-    $retrieve = 1 unless defined $retrieve;
-
-    if ( $retrieve and not $stack[$$self] ) {
-        $stack[$$self] = $self->ohloh->fetch_account_stack( $self->id );
-        $stack[$$self]->set_account($self);
-    }
-
-    return $stack[$$self];
-}
-
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-sub sent_kudos {
-    my $self = shift;
-
-    $kudos_of[$$self] ||= $self->ohloh->get_kudos( id => $self->id );
-
-    return $kudos_of[$$self]->sent;
-}
-
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-sub received_kudos {
-    my $self = shift;
-
-    $kudos_of[$$self] ||= $self->ohloh->get_kudos( id => $self->id );
-
-    return $kudos_of[$$self]->received;
-}
-
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-sub kudos {
-    my $self = shift;
-
-    return $kudos_of[$$self] ||= $self->ohloh->fetch_kudos( $self->id );
-}
-
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-'end of WWW::Ohloh::API::Account';
-__END__
-
-=head1 NAME
-
-WWW::Ohloh::API::Account - an Ohloh account
-
-=head1 SYNOPSIS
-
-    use WWW::Ohloh::API;
-
-    my $ohloh = WWW::Ohloh::API->new( api_key => $my_api_key );
-    my $account $ohloh->fetch_account( 12933 );
-
-    print $account->name;
 
 =head1 DESCRIPTION
 
-W::O::A::Account contains the information associated with an Ohloh 
-account as defined at http://www.ohloh.net/api/reference/account. 
-To be properly populated, it must be created via
-the C<get_account> method of a L<WWW::Ohloh::API> object.
-
-=head1 METHODS 
-
-=head2 API Data Accessors
-
-
-
-
-
-
-=head3 kudoScore, kudo_score, kudo
-
-Return a L<WWW::Ohloh::API::KudoScore> object holding the account's 
-kudo information, or I<undef> if the account doesn't have a kudo score
-yet. All three methods are equivalent.
-
-=head3 stack( $retrieve )
-
-Return the stack associated with the account as a
-L<WWW::Ohloh::API::Stack> object.
-
-If the optional I<$retrieve> argument is given and false,
-the stack will not be queried from the Ohloh server and,
-if the information has not been retrieved previously, the method
-will return nothing.
-
-=head2 Other Methods
+C<WWW::Ohloh::API::Object::Account>
+contains the information associated with an Ohloh 
+account as defined at L<http://www.ohloh.net/api/reference/account>. 
 
 =head1 OVERLOADING
 
@@ -362,10 +243,6 @@ the name associated with the account. E.g.,
 
 =over
 
-=item * 
-
-L<WWW::Ohloh::API>, L<WWW::Ohloh::API::KudoScore>.
-
 =item *
 
 Ohloh API reference: http://www.ohloh.net/api/getting_started
@@ -375,32 +252,4 @@ Ohloh API reference: http://www.ohloh.net/api/getting_started
 Ohloh Account API reference: http://www.ohloh.net/api/reference/account
 
 =back
-
-=head1 VERSION
-
-This document describes WWW::Ohloh::API version 1.0_1
-
-=head1 BUGS AND LIMITATIONS
-
-WWW::Ohloh::API is very extremely alpha quality. It'll improve,
-but till then: I<Caveat emptor>.
-
-The C<as_xml()> method returns a re-encoding of the account data, which
-can differ of the original xml document sent by the Ohloh server.
-
-Please report any bugs or feature requests to
-C<bug-www-ohloh-api@rt.cpan.org>, or through the web interface at
-L<http://rt.cpan.org>.
-
-
-=head1 AUTHOR
-
-Yanick Champoux  C<< <yanick@cpan.org> >>
-
-=head1 LICENCE AND COPYRIGHT
-
-Copyright (c) 2008, Yanick Champoux C<< <yanick@cpan.org> >>. All rights reserved.
-
-This module is free software; you can redistribute it and/or
-modify it under the same terms as Perl itself. See L<perlartistic>.
 
